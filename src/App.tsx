dea8f241cb3b83a9d0712a65efa0e26889e3f380
dibborah <dibyajyotiborah469@gmail.com>
2024-02-22 20:20:31 +0530

    simple form with simple validation in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
+// Create a Simple Form in React
+
+import { useState } from "react"
+
 
 const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<{email: string, password: string}>({
+    email: "",
+    password: ""
+  });
+  
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrors({email: '', password: ''});
+
+    if(!email.includes('@')) {
+      setErrors({...errors, email: 'The email must include @'});
+      return;
+    }
+    if(password.length < 8){
+      setErrors({...errors, password: "The length of password must be greater than 8"});
+      return;
+    }
+    // Form Submited
+    console.log("Form is submitted");
+  }
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      {errors.email && (
+        <div style={{color: "red"}}> {errors.email}</div>
+      )}
+      <div>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors.password && (
+        <div style={{color: "red"}}> {errors.password}</div>
+      )}
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
